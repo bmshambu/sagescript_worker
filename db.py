fbb5bf938f814603bcf9ee9a4c9d3aa97c0bea7d
bmshambu <bmshambu134@gmail.com>
2026-01-29 18:12:02 +0530

    db url

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
 import os
 import psycopg
 from psycopg.rows import dict_row
-from config import AppConfig
-cfg =AppConfig()
+#from config import AppConfig
+#cfg =AppConfig()
+import os
 def get_connection():
     return psycopg.connect(
-        cfg.database_url,
+        os.environ["database_url"],
         row_factory=dict_row
     )
